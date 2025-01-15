4287024c7f3a530b351bde72cb368a4e03371ce1
Riz-7595 <rizwankhalid7595@gmail.com>
2025-01-15 17:43:50 +0500

    setup tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,32 +3,32 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <div>
-            Hello World!
-        </div>
-        <Footer>
-            Rizwan
-        </Footer>
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+      <Footer>
+        Rizwan
+      </Footer>
     </div>
   )
-} 
-
-const Footer = (props) => {
-    console.log('Hello')
-
-    const o = false;
-
-
-    return(
-        <div>
-            <p>My name is {props.children}.</p>
-            {
-                o ? 'ABC' : 'XYZ'
-            }
-
-        </div>
-
-    )
 }
 
-export {Header}
+const Footer = (props) => {
+  console.log('Hello')
+
+  const o = false;
+
+
+    return (
+    <div>
+      <p>My name is {props.children}.</p>
+      {
+        o ? 'ABC' : 'XYZ'
+      }
+
+    </div>
+
+  )
+}
+
+export { Header }
