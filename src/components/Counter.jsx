44b467cf14796cbcfb0926261a7ad99f46102131
Riz-7595 <rizwankhalid7595@gmail.com>
2025-01-17 18:56:39 +0500

    useDispatch practice

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,9 +7,10 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   return (
+
     <div className='flex mt-2 gap-4'>
-      <button onClick={()=>{dispatch(decrement())}} className='pb-1 w-8 bg-green-300 rounded-xl text-xl'>-</button>
-      <button onClick={()=>{dispatch(increment())}} className='pb-1 w-8 bg-green-500 rounded-xl text-xl'>+</button>
+      <button onClick={() => { dispatch(decrement()) }} className='pb-1 w-8 bg-green-300 rounded-xl text-xl'>-</button>
+      <button onClick={() => { dispatch(increment()) }} className='pb-1 w-8 bg-green-500 rounded-xl text-xl'>+</button>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Counter from '../components/Counter';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementByAmount, decrementByAmount } from '../app/counterSlice';
 
 const HomePage = () => {
 
-  const count = useSelector((state)=>state.counter.value)
+  const count = useSelector((state) => state.counter.value)
+
+  const dispatch = useDispatch()
+
+  const [amount, setAmount] = useState(0)
 
   const [color, setColor] = useState("bg-gray-300")
 
@@ -17,22 +22,32 @@ const HomePage = () => {
   }
 
   class Person {
-    constructor(first, last = '')
-    {
+    constructor(first, last = '') {
       this.firstName = first;
       this.lastName = last;
     }
   }
 
   class Student extends Person {
-    constructor(first, last='',degree)
-    {
-      super(first,last);
+    constructor(first, last = '', degree) {
+      super(first, last);
       this.degreeName = degree;
     }
   }
 
-  const me = new Student('Rizwan','Khalid','COMSATS');
+  const addAmount = () => {
+    dispatch(incrementByAmount(Number(amount)));
+    setAmount(0);
+
+  }
+
+  const subtractAmount = () => {
+    dispatch(decrementByAmount(Number(amount)));
+    setAmount(0);
+
+  }
+
+  const me = new Student('Rizwan', 'Khalid', 'COMSATS');
 
   return (
     <div className='px-12 pt-12'>
@@ -41,13 +56,18 @@ const HomePage = () => {
         <Link to='/B' className={`px-8 py-4 rounded-xl ${color}`}>B</Link>
         <Link to='/C' className={`px-8 py-4 rounded-xl ${color}`}>C</Link>
       </nav>
-      <Header address={address} student={me} className='text-blue-500'/>
+      <Header address={address} student={me} className='text-blue-500' />
       <div className='flex gap-1'>
-      <button onClick={()=>{setColor('bg-blue-300')}} className='bg-blue-300 rounded-xl p-4 mt-6'>Button A</button>
-      <button onClick={()=>{setColor('bg-red-300')}} className='bg-red-300 rounded-xl p-4 mt-6'>Button B</button>
+        <button onClick={() => { setColor('bg-blue-300') }} className='bg-blue-300 rounded-xl p-4 mt-6'>Button A</button>
+        <button onClick={() => { setColor('bg-red-300') }} className='bg-red-300 rounded-xl p-4 mt-6'>Button B</button>
       </div>
-      <Counter/>
+      <Counter />
       <p>Counter: {count}</p>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value={amount ? amount : ''} onChange={(e) => setAmount(e.target.value)} className='bg-orange-300 rounded-lg px-4 pb-1 w-32 mr-4' />
+        <button onClick={addAmount} className='mr-4'>Add</button>
+        <button onClick={subtractAmount}>Subtract</button>
+      </form>
     </div>
   );
 }
