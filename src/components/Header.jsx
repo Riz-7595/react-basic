fb92e554b2f8d0efacce7d67f2ec3b822dd5dfc0
Riz-7595 <rizwan.khalid@epochclan.com>
2025-01-28 17:55:11 +0500

    add manual darkMode using className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = (props) => {
   const { className } = props
   const { firstName, lastName } = props.student;
   return (
-    <div>
-      <h1 className={`text-3xl font-bold underline p-2 bg-blue-100 flex justify-center items-center ${className}`}>
+    <div className='dark'>
+      <h1 className={`dark:bg-black dark:text-white text-3xl font-bold underline p-2 bg-blue-100 flex justify-center items-center ${className}`}>
         Hello World!
       </h1>
       <Body address={props.address}>
